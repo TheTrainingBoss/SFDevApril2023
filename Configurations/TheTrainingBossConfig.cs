8e3f8c716efe716316905b03ee8f51fb1a1f50d9
Lino Tadros <thetrainingboss@gmail.com>
2023-04-21 13:56:11 -0400

    Confi Sections and EventHub

---
+++ Configurations/TheTrainingBossConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using Telerik.Sitefinity.Configuration;
+using Telerik.Sitefinity.Localization;
+
+namespace SFDevApril2023.Configurations
+{
+    public class TheTrainingBossConfig: ConfigSection
+    {
+        [ObjectInfo(Title = "FaceBookUsername", Description = "Please enter a FaceBookUsername")]
+        [ConfigurationProperty("FaceBookUsername", DefaultValue = "")]
+        public string FaceBookUsernameProperty
+        {
+            get
+            {
+                return (string)this["FaceBookUsername"];
+            }
+            set
+            {
+                this["FaceBookUsername"] = value;
+            }
+        }
+
+        [ObjectInfo(Title = "FaceBookPassword", Description = "This is a FaceBookPassword field.")]
+        [ConfigurationProperty("FaceBookPassword", DefaultValue = "")]
+        public string FaceBookPasswordProperty
+        {
+            get
+            {
+                return (string)this["FaceBookPassword"];
+            }
+            set
+            {
+                this["FaceBookPassword"] = value;
+            }
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SFDevApril2023.Global" Language="C#" %>
